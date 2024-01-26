a177238fd83d67e4d519cfcb160f863cbd8a2b42
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-26 11:05:48 -0600

    [ADD] Carousel on landing page

--- src/components/organisms/FeatureSection.jsx
+++ src/components/organisms/FeatureSection.jsx
@@ -2,31 +2,30 @@ import PropTypes from 'prop-types';
 
 const FeatureSection = () => {
     return (
-        <section className="w-full py-12 md:py-24 lg:py-32">
-            <div className="container space-y-12 px-4 md:px-6">
+        <section className="w-full py-12 md:py-24 lg:py-32 flex justify-center items-center">
+            <div className="container space-y-12 px-4 md:px-6 flex justify-center items-center">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
                         <div className="inline-block rounded-lg bg-gray-800 px-3 py-1 text-sm text-neon">
-                            Featured Playlists
+                            Listas de Reproducción Destacadas
                         </div>
                         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-neon">
-                            Discover Weekly
+                            Descubrimiento Semanal
                         </h2>
                         <p className="max-w-[900px] text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                            Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts chosen just for you. Updated every Monday.
+                            Tu mezcla semanal de música fresca. Disfruta de nuevos descubrimientos y selecciones profundas elegidas solo para ti. Actualizado cada lunes.
                         </p>
                     </div>
                 </div>
                 <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
                     <FeatureItem
-                        title="New Releases"
-                        description="Stay updated with the latest hits."
+                        title="Nuevos Lanzamientos"
+                        description="Mantente actualizado con los últimos éxitos."
                     />
                     <FeatureItem
-                        title="Recommended Artists"
-                        description="Explore artists based on your music taste."
+                        title="Artistas Recomendados"
+                        description="Explora artistas basados en tus gustos musicales."
                     />
-                    {/* Agrega más FeatureItem aquí según sea necesario */}
                 </div>
             </div>
         </section>
--- src/components/organisms/MainSection.jsx
+++ src/components/organisms/MainSection.jsx
@@ -1,4 +1,37 @@
+import { useState, useEffect, useRef, useCallback } from 'react';
+
+import billieEilishImage from '../../assets/billie_eilish_happier_than_ever.jpg';
+import smithereensImage from '../../assets/smithereens.jpg';
+import ameriImage from '../../assets/duki_antes_de_ameri.jpg';
+import loverImage from '../../assets/taylor_swift_lover.jpg';
+
 const MainSection = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fade, setFade] = useState(false);
+    const images = [billieEilishImage, smithereensImage, loverImage, ameriImage];
+    const slideRef = useRef();
+
+    const handleSlide = useCallback((newIndex) => {
+        setFade(true);
+        setTimeout(() => {
+            if (newIndex < 0) {
+                setCurrentIndex(images.length - 1);
+            } else if (newIndex >= images.length) {
+                setCurrentIndex(0);
+            } else {
+                setCurrentIndex(newIndex);
+            }
+            setFade(false);
+        }, 500); // Duración de la transición de opacidad
+    }, [images.length]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            handleSlide(currentIndex + 1);
+        }, 4000);
+        return () => clearInterval(interval);
+    }, [currentIndex, images.length, handleSlide]);
+
     return (
         <main className="flex-1">
             <section className="w-full pt-12 md:pt-24 lg:pt-32 border-y border-gray-700">
@@ -6,20 +39,28 @@ const MainSection = () => {
                     <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
                         <div>
                             <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] text-neon">
-                                Stream Your Favorite Music
+                                Escucha tu música favorita
                             </h1>
                             <p className="mx-auto max-w-[700px] text-gray-400 md:text-xl">
-                                Discover and listen to your favorite songs, albums, and playlists from all around the world.
+                                Descubre y escucha tus canciones, álbumes y listas de reproducción favoritos de todo el mundo.
                             </p>
                         </div>
-                        <div className="flex flex-col items-start space-y-4">
-                            <img
-                                alt="Music"
-                                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
-                                height="310"
-                                src="/placeholder.svg"
-                                width="550"
-                            />
+                        <div className="flex flex-col items-center space-y-4">
+                            {/* Carrusel */}
+                            <div className="relative overflow-hidden" style={{ width: '500px', height: '500px' }}>
+                                <div className="absolute top-0 left-0 w-full h-full flex justify-between items-center">
+                                    <button onClick={() => handleSlide(currentIndex - 1)} className="text-white">&#10094;</button>
+                                    <button onClick={() => handleSlide(currentIndex + 1)} className="text-white">&#10095;</button>
+                                </div>
+                                <div ref={slideRef}>
+                                    <img
+                                        src={images[currentIndex]}
+                                        alt={`Imagen ${currentIndex + 1}`}
+                                        className={`object-cover rounded-xl transition-opacity ${fade ? 'opacity-0' : 'opacity-100'}`}
+                                        style={{ width: '500px', height: '500px' }}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
